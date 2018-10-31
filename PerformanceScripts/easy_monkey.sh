9efd3a2245f32f30777a3a4bcf10e03fa01f1ed4
shengjie.liu <shengjie.liu@542deMacBook-Air.local>
2018-10-31 15:53:41 +0800

    update

--- PerformanceScripts/easy_monkey.sh
+++ PerformanceScripts/easy_monkey.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 # Author: Shengjie.Liu
-# Date: 2018-10-26
-# Version: 1.0
+# Date: 2018-10-31
+# Version: 1.1
 # Description: script of monkey
 # How to use: sh +x easy_monkey.sh <packagename> <extime>
 
 packagename=$1
 echo "应用包名：$packagename"
+
 extime=$2
-echo "执行次数：$extime"
+case $extime in
+    1)  extime=54000
+    ;;
+    2)  extime=108000
+    ;;
+    8)  extime=432000
+    ;;
+    *)  echo "执行次数：$extime"
+    ;;
+esac
 
 init_data(){
 	if [[ ! -d $OUTPUT ]]; then
@@ -20,8 +30,12 @@ WORKSPACE=`pwd`
 OUTPUT=${WORKSPACE}/output_monkey
 
 init_data
+echo "开始时间：`date`"
+# adb shell monkey -p ${packagename} --ignore-crashes --ignore-timeouts --ignore-security-exceptions \
+# -s 1024 --throttle 200 -v ${extime} 1>$OUTPUT/monkey_log.txt 2>$OUTPUT/error.txt
 
-adb shell monkey -p ${packagename} --ignore-crashes --ignore-timeouts --ignore-security-exceptions \
+adb shell monkey -p ${packagename} --pct-touch 40 --pct-motion 25 --pct-appswitch 10 --pct-rotation 5 \
+--ignore-crashes --ignore-timeouts --ignore-security-exceptions \
 -s 1024 --throttle 200 -v ${extime} 1>$OUTPUT/monkey_log.txt 2>$OUTPUT/error.txt
 
 showerror(){
