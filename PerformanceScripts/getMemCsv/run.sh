3864af1038abd6a7e4a9ed41a48be1b2080218c8
shengjie.liu <shengjie.liu@542deMacBook-Air.local>
2018-12-24 17:51:11 +0800

    optimize code

--- PerformanceScripts/getMemCsv/run.sh
+++ PerformanceScripts/getMemCsv/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+# How to use: sh +x run.sh <meminfo.txt path>
 
 MEMINFO=$1
 CURRENT_TIME=`date +%Y%m%d%H%M`
 
 report_memory_info()
 {
-    sh +x memory_report.sh ${MEMINFO} $CURRENT_TIME
+    sh +x memory_report.sh ${MEMINFO} ${CURRENT_TIME}
     cp -p logs/csv/t_u.csv meminfo.csv
 }
 
