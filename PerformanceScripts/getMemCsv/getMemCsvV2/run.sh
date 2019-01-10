7607ab694bddb7e8a08e945c9f8b2550c34e008b
Shengjie <liushengchieh@gmail.com>
2019-01-10 19:31:03 +0800

    解析内存文件

---
+++ PerformanceScripts/getMemCsv/getMemCsvV2/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# function: parse meminfo.txt
+# How to use: sh +x run.sh <meminfo.txt path>
+
+init_data()
+{
+    if [[ ! -d ${CURRENT_OUTPUT} ]];then
+        mkdir -p ${CURRENT_OUTPUT}
+    fi
+}
+
+report_memory_info()
+{
+    sh +x memory_report.sh ${1}
+    cp -p logs/csv/t_u.csv ${2}
+}
+
+MEMINFO=$1
+CURRENT_TIME=`date +%Y%m%d%H%M`
+WORKSPACE=`pwd`
+OUTPUT=${WORKSPACE}/output_csv
+CURRENT_OUTPUT=${OUTPUT}/${CURRENT_TIME}
+REPORT=${CURRENT_OUTPUT}/meminfo.csv
+
+init_data
+report_memory_info ${MEMINFO} ${REPORT}
+rm -r logs
+echo "Report: ${REPORT}"
+echo "Success"
