9bb7dd114a72d5830843f7a20b93b936ce27ca6d
Shengjie <liushengchieh@gmail.com>
2019-02-22 17:43:26 +0800

    格式修改

--- launchable_activity.sh
+++ launchable_activity.sh
@@ -3,12 +3,12 @@
 
 echo "apk path:"
 read apk_path
-launch_activity=$(aapt dump badging $apk_path | grep launchable-activity | sed 's/ //g' | tr -d $'\r' | cut -d"'" -f2) 
-packagename=$(aapt dump badging $apk_path | grep package: | sed 's/ //g' | tr -d $'\r' | cut -d"'" -f2)
-if [ ! -n "$launch_activity" ]; then
+launch_activity=$(aapt dump badging ${apk_path} | grep launchable-activity | sed 's/ //g' | tr -d $'\r' | cut -d"'" -f2)
+package_name=$(aapt dump badging ${apk_path} | grep package: | sed 's/ //g' | tr -d $'\r' | cut -d"'" -f2)
+if [[ ! -n "${launch_activity}" ]]; then
 	echo "Sorry, here is not have information about launchable-activity."
 else
-    echo $launch_activity
-    echo $packagename
-    echo "${packagename}/${launch_activity}"
+    echo ${launch_activity}
+    echo ${package_name}
+    echo "${package_name}/${launch_activity}"
 fi
