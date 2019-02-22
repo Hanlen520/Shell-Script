ef475fc7dff0d05385bd79e958707362917669d0
Shengjie <liushengchieh@gmail.com>
2019-02-22 10:09:22 +0800

    视频录制后输出的文件夹修改

--- screen_recording.sh
+++ screen_recording.sh
@@ -2,5 +2,5 @@
 
 echo "Please input the video name:"
 read videoname
-adb shell screenrecord --time-limit 30 /sdcard/$videoname.mp4
-adb pull /sdcard/$videoname.mp4 ~/Desktop/video/
+adb shell screenrecord --time-limit 30 /sdcard/${videoname}.mp4
+adb pull /sdcard/${videoname}.mp4 ~/Downloads/
