5bb7553f2ec97f870f91e11f538fc4b6750717ac
Shengjie <liushengchieh@gmail.com>
2019-02-22 11:08:20 +0800

    修改log命令过滤方式

--- grab_log.sh
+++ grab_log.sh
@@ -4,16 +4,13 @@
 DATE=$(date "+%Y%m%d%H%M%S")
 # 读取包名
 echo -n "Please enter the package name:"
-read tag
-# 读取log文件名
-# echo -n "Please enter the app name:"
-# read app_name
+read package_name
 # 日志级别：V/D/I/W/E/F/S
 echo -n "Please enter the priority of log:"
 read priority
 # log命令
-adb logcat -d -v time "${tag}:${priority}" > ~/Desktop/logg/${tag}${DATE}.log
-adb logcat -d -v time > ~/Desktop/logg/${tag}${DATE}_all.log
+adb logcat -d -v long "AndroidRuntime:${priority}" "*:S" > ~/Desktop/logg/${package_name}${DATE}.log
+adb logcat -d -v threadtime > ~/Desktop/logg/${package_name}${DATE}_all.log
 # anr日志
 adb pull /data/anr/traces.txt ~/Desktop/logg/
 # 清空日志
