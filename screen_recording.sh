c0a29fec373c1ebe6d29f84bffcf65c97372534d
Shengjie <liushengchieh@gmail.com>
2019-03-26 15:41:29 +0800

    add video time

--- screen_recording.sh
+++ screen_recording.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+echo "Please input the vieo time:"
+read videotime
 echo "Please input the video name:"
 read videoname
-adb shell screenrecord --time-limit 30 /sdcard/${videoname}.mp4
+adb shell screenrecord --time-limit ${videotime} /sdcard/${videoname}.mp4
 adb pull /sdcard/${videoname}.mp4 ~/Downloads/
