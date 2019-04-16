d3954cba84d3a05a7b9ea0090fe683de78cb5fb3
Shengjie <liushengchieh@gmail.com>
2019-04-16 10:05:12 +0800

    新增权限检查脚本

---
+++ permission_check.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+# Author: Shengjie.Liu
+# Date: 2019-04-15
+# Version: 1.0
+# Description: 权限检查
+# How to use: sh +x permission_check.sh
+
+#清空上次运行后产生的文件
+if [[ -f permission_old.txt ]]; then
+    rm permission_old.txt permission_new.txt
+fi
+
+#读取旧版本apk文件地址
+read -p "请输入上个版本apk文件存放地址：" apk_old
+read -p "请输入最新版本apk文件存放地址：" apk_new
+
+#aapt命令解析apk,输出权限到文件
+aapt d badging ${apk_old} | grep "uses-permission:" | awk -F "'" '{print $2}' > permission_old.txt
+aapt d badging ${apk_new} | grep "uses-permission:" | awk -F "'" '{print $2}' > permission_new.txt
+
+#遍历新版本权限列表，对比旧版本权限列表是否相同，不同则为新增
+for x in $(cat permission_new.txt); do
+    if cat permission_old.txt | grep ${x} > /dev/null; then
+        echo "hello, world" > /dev/null
+    else
+        echo ${x} >> permission_increase.txt
+    fi
+done
+
+#遍历旧版本权限列表，对比新版本权限列表是否相同，不同则为新减少
+for y in $(cat permission_old.txt); do
+    if cat permission_new.txt | grep ${y} > /dev/null; then
+        echo "hello, world" > /dev/null
+    else
+        echo ${y} >> permission_decrease.txt
+    fi
+done
+
+#判断permission_increase.txt是否存在：存在，输出新增权限提醒；不存在，输出无新增权限
+if [[ ! -f permission_increase.txt ]]; then
+    echo "无新增权限"
+else
+    echo "新增权限："
+    cat permission_increase.txt
+    #删除新增权限文件
+    rm permission_increase.txt
+fi
+
+#判断permission_decrease.txt是否存在：存在，输出新减少权限提醒；不存在，输出无新减少权限
+if [[ ! -f permission_decrease.txt ]]; then
+    echo "无新减少权限"
+else
+    echo "新减少权限："
+    cat permission_decrease.txt
+    #删除新减少权限文件
+    rm permission_decrease.txt
+fi
