06143136a082b95791ad7d5160c09ff7ff5400a8
Shengjie <liushengchieh@gmail.com>
2019-05-06 11:07:56 +0800

    delete log

--- apksize_check.sh
+++ apksize_check.sh
@@ -6,18 +6,12 @@ read -p "请输入最新版本apk文件存放地址：" apk_new
 
 #检查apk size
 b_size_old=`ls -l ${apk_old} | awk '{print $5}'`
-echo $b_size_old
 k_size_old=`awk 'BEGIN{printf "%.2f\n", "'${b_size_old}'"/'1024'}'`
-echo $k_size_old
 m_size_old=`awk 'BEGIN{printf "%.2f\n", "'${k_size_old}'"/'1024'}'`
-echo $m_size_old
 
 b_size_new=`ls -l ${apk_new} | awk '{print $5}'`
-echo $b_size_new
 k_size_new=`awk 'BEGIN{printf "%.2f\n", "'${b_size_new}'"/'1024'}'`
-echo $k_size_new
 m_size_new=`awk 'BEGIN{printf "%.2f\n", "'${k_size_new}'"/'1024'}'`
-echo $m_size_new
 
 #输出apk size
 echo "------"
