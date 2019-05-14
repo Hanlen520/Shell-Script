cb28b7ae306031b60813bddfc25f5033d6cb9029
Shengjie <liushengchieh@gmail.com>
2019-05-14 14:50:14 +0800

    add resolution and density

--- device_info.sh
+++ device_info.sh
@@ -9,3 +9,18 @@ adb shell getprop ro.product.brand
 echo "系统版本："
 adb shell getprop ro.build.version.release
 
+release=$(adb shell getprop ro.build.version.release | sed 's/ //g' | tr -d $'\r')
+release_one=$(echo ${release} | awk -F. '{print $1}')
+
+# screen resolution/density
+if [[ ${release_one} > 4 ]]; then
+    density=$(adb shell wm density | tr -d $'\r' | awk '{print $3}')
+    size=$(adb shell wm size | tr -d $'\r' | awk '{print $3}')
+    display="${size}/${density}dpi"
+else
+    size=`adb shell dumpsys window displays | grep "init" | tr -d $'\r' | awk '{print $1}' | cut -d"=" -f 2`
+    density=`adb shell dumpsys window displays | grep "init" | tr -d $'\r' | awk '{print $2}'`
+    display="${size}/${density}"
+fi
+
+echo $display
