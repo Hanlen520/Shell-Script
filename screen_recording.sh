999a76cf6513ad1a0c3049ca89acb2539b23c4ca
Shengjie <liushengchieh@gmail.com>
2019-05-23 11:51:08 +0800

    添加截屏语句

--- screen_recording.sh
+++ screen_recording.sh
@@ -5,3 +5,10 @@ echo "Please input the video name:"
 read videoname
 adb shell screenrecord --time-limit ${videotime} /sdcard/${videoname}.mp4
 adb pull /sdcard/${videoname}.mp4 ~/Downloads/
+
+
+#adb exec-out screencap -p > sc.png
+#adb shell screencap -p /sdcard/sc.png
+#adb pull /sdcard/sc.png
+#adb shell screencap -p | gsed "s/\r$//" > sc.png
+#brew install gnu-sed
